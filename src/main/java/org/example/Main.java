57b3a31b25ab693ca9144779a6f88448f159e95d
Tadeusz Kamyszek <tkamyszek@op.pl>
2024-01-25 21:37:02 +0100

    Task3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Square square = new Square(5);
+        Circle circle = new Circle(5);
+
+        System.out.println("square area " + square.getArea());
+        System.out.println("square perimeter " + square.getPerimeter());
+
+        System.out.println("circle area " + circle.getArea());
+        System.out.println("circle perimeter " + circle.getPerimeter());
+
     }
 }
