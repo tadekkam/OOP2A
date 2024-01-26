db6f4556aba1c26ce94e7284185316d63fbc790a
Tadeusz Kamyszek <tkamyszek@op.pl>
2024-01-26 07:37:48 +0100

    Task5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Dog dog = new Dog("Puszek", "dachshund");
+        System.out.println("Name: " + dog.getName());
+        System.out.println("Breed: " + dog.getBreed());
+
+        Dog dogNew = new Dog();
+        dogNew.setName("Okruszek");
+        dogNew.setBreed("wolfhound");
+        System.out.println("Name: " + dogNew.getName());
+        System.out.println("Breed: " + dogNew.getBreed());
+
     }
 }
