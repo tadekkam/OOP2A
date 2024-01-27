5a6055067309d8ec11081bc961b31afdb170eed7
Tadeusz Kamyszek <tkamyszek@op.pl>
2024-01-27 22:16:36 +0100

    Task8

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.Objects;
+
+public class Person {
+
+    String name;
+    String surname;
+    int age;
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(name, person.name) && Objects.equals(surname, person.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, age);
+    }
+}
